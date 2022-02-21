14738adde49fe5b6b315361c715569b2cb716252
Yuya Kono <takohati0821@gmail.com>
2022-02-21 11:50:07 +0900

    Rename parser to nom_parser

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 mod ast;
-mod parser;
-
+mod nom_parser;
 use std::io::{self, Read};
 
 fn main() -> io::Result<()> {
     // Read from stdin
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
-    println!("{:?}", parser::parse(&input));
+    println!("{:?}", nom_parser::parse(&input));
     Ok(())
 }
